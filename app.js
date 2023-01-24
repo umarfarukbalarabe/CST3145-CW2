1f3ef6b4c101758a557c96632f03f5327008c7a1
Umar Faruk Balarabe <umarfarookb123@gmail.com>
2023-01-24 23:52:40 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+let lessons = [
+    {'topic': 'chemistry','location':'Sharjah','price':'100'},
+    {'topic': 'chemistry','location':'Abu Dhabi','price':'90'},
+    {'topic': 'chemistry','location':'Dubai','price':'80'},
+    {'topic': 'chemistry','location':'Dubai','price':'150'},
+];
+
+let user = [
+    {'email': 'user@email.com','password':'mypassword'}
+]
+
+app.get('/lessons', (request, response) => {
+    response.json(lessons);
+});
+
+app.get('/user', (request, response) => {
+    response.json(user);
+});
+
+app.use(express.static("./"));
+
+app.listen(3000);
